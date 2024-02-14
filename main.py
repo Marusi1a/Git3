2e1d7fb6fe74d566a7c9973ea89eae4b20315668
Nastyaknu <zimanastya@knu.ua>
2024-02-14 23:31:27 +0200

    chenging main.py n

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ if is_power_of_four(user_input):
     print(f"{user_input} є ступенем 4.")
 else:
     print(f"{user_input} не є ступенем 4.")
+    
+from utils.py import unique_n
+
+a, b = map(int, input().split())
+result = [str(num) for num in range(a, b + 1) if unique_n(num)]
+
+print(*result)
