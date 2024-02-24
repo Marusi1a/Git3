ba11a3a7b6ef368ff9412565edb6c2ae0f6a7ad3
pogrebets <159121556+pogrebets@users.noreply.github.com>
2024-02-24 22:38:53 +0200

    gf

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ if is_power_of_four(user_input):
     print(f"{user_input} є ступенем 4.")
 else:
     print(f"{user_input} не є ступенем 4.")
+
+from untils.py import check_even_add
+num = 72
+result = check_even_odd(num)
+print(f"{num} - {result})
+
+num = int(input("Введіть число: "))
+result = check_even_odd(num)
+print(f"{num} - {result}.")
