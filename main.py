62aff2a8c3955a36f513105b5aab675c1d0c0ad7
Marusi1a <marusischasodiyka@gmail.com>
2024-02-24 22:06:44 +0200

    commit

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ if is_power_of_four(user_input):
     print(f"{user_input} є ступенем 4.")
 else:
     print(f"{user_input} не є ступенем 4.")
+
+from utils.py import find_gcd
+num1 = int(input("Введіть перше число: "))
+num2 = int(input("Введіть друге число: "))
+
+gcd = find_gcd(num1, num2)
+
+print(f"НСД({num1}, {num2}) = {gcd}")
